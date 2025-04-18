4a5c65d6dfdf0528aa0d00a96dc738b3485a0b62
0cl0ck <hugodewas@gmail.com>
2025-04-18 13:20:46 +0200

    feat: Ajout de la page Articles en statique, conforme à la maquette

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 // Routing
 import { AppRoutingModule } from './app-routing.module';
@@ -17,9 +20,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, RegisterComponent, LoginComponent],
+  declarations: [AppComponent, HomeComponent, RegisterComponent, LoginComponent, ArticlesComponent],
   imports: [
     // Angular core modules
     BrowserModule,
@@ -32,6 +36,9 @@ import { LoginComponent } from './pages/auth/login/login.component';
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
+    MatMenuModule,
+    MatCardModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ front/src/app/pages/articles/articles.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Article {
+  id: number;
+  title: string;
+  content: string;
+  date: Date;
+  author: string;
+}
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.scss']
+})
+export class ArticlesComponent implements OnInit {
+  // Mock articles data
+  articles: Article[] = [
+    {
+      id: 1,
+      title: 'Titre de l\'article',
+      content: 'Content: lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled...',
+      date: new Date(),
+      author: 'Auteur'
+    },
+    {
+      id: 2,
+      title: 'Titre de l\'article',
+      content: 'Content: lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled...',
+      date: new Date(Date.now() - 86400000), // Yesterday
+      author: 'Auteur'
+    },
+    {
+      id: 3,
+      title: 'Titre de l\'article',
+      content: 'Content: lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled...',
+      date: new Date(Date.now() - 2 * 86400000), // 2 days ago
+      author: 'Auteur'
+    },
+    {
+      id: 4,
+      title: 'Titre de l\'article',
+      content: 'Content: lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled...',
+      date: new Date(Date.now() - 3 * 86400000), // 3 days ago
+      author: 'Auteur'
+    }
+  ];
+
+  sortOrder: 'newest' | 'oldest' = 'newest'; // Default sort order
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.sortArticles();
+  }
+
+  sortArticles() {
+    if (this.sortOrder === 'newest') {
+      this.articles.sort((a, b) => b.date.getTime() - a.date.getTime());
+    } else {
+      this.articles.sort((a, b) => a.date.getTime() - b.date.getTime());
+    }
+  }
+
+  changeSortOrder(order: 'newest' | 'oldest') {
+    this.sortOrder = order;
+    this.sortArticles();
+  }
+
+  navigateToArticleDetail(articleId: number) {
+    // To be implemented when article detail page is created
+    console.log(`Navigating to article ${articleId}`);
+  }
+
+  createNewArticle() {
+    // To be implemented when article creation page is created
+    console.log('Creating new article');
+  }
+
+  navigateToThemes() {
+    // To be implemented when themes page is created
+    console.log('Navigating to themes');
+  }
+
+  navigateToProfile() {
+    // To be implemented when profile page is created
+    console.log('Navigating to profile');
+  }
+
+  logout() {
+    // Mock logout functionality
+    console.log('Logging out');
+    this.router.navigate(['/']);
+  }
+
+  toggleSortDirection() {
+    // Toggle between newest and oldest
+    this.changeSortOrder(this.sortOrder === 'newest' ? 'oldest' : 'newest');
+  }
+}
